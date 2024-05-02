da1a56153274ed11b43d1293967eb4d0c9e70b7e
NAlejandra05 <alejandraunas96@gmail.com>
2024-05-02 18:41:23 -0500

    actualizado

--- js/Registro.js
+++ js/Registro.js
@@ -1,7 +1,7 @@
-// Selección del formulario por su id
+
 const form = document.getElementById('productForm');
 
-// Función para limpiar los campos del formulario
+
 function limpiarCampos() {
   document.getElementById('titulo').value = '';
   document.getElementById('Autor').value = '';
@@ -13,11 +13,11 @@ function limpiarCampos() {
   document.getElementById('imagen').value = '';
 }
 
-// Agregar un evento de escucha al evento submit del formulario
-form.addEventListener('submit', (e) => {
-  e.preventDefault(); // Previene el envío predeterminado del formulario
 
-  // Obtener los valores de los campos del formulario
+form.addEventListener('submit', (e) => {
+  e.preventDefault(); 
+
+  
   const titulo = document.getElementById('titulo').value;
   const autor = document.getElementById('Autor').value;
   const genero = document.getElementById('genero').value;
@@ -27,11 +27,11 @@ form.addEventListener('submit', (e) => {
   const añoPublicacion = parseInt(document.getElementById('añoPublicacion').value);
   const imagen = document.getElementById('imagen').value;
 
-  // Aquí iría tu lógica de validación y manipulación de datos...
+ 
 
-  // Restablecer el formulario y limpiar el mensaje
-  form.reset(); // Esto restablecerá el formulario
-  // mensajeElement.textContent = ''; // Asegúrate de definir mensajeElement
+  
+  form.reset();
+  // mensajeElement.textContent = ''; 
 
   
   setTimeout(() => {
